6b382b869c7fa325686e3bdf0cb744b9216a5e7e
stutisaurya <stuti.saurya@gmail.com>
2021-07-06 23:25:23 +0530

    Added:ValidateEmail

---
+++ src/com/Email.java
@@ -0,0 +1,20 @@
+package com;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Email {
+	public static void main(String[] args) {
+		String email = "abc";
+		String regex = "^[a-zA-Z0-9]+$";
+
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		boolean result = matcher.matches();
+		if (result)
+			System.out.println("Vaild Email");
+		else
+			System.out.println("InVaild Email");
+	}
+
+}
